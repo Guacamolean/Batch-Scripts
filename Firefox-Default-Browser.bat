045ac450dd44ccabc4d79788f1b1efbc6b325fff
Guacamolean <mummyandachainsaw@gmail.com>
2017-06-14 12:33:27 -0400

    Initial upload

---
+++ Firefox-Default-Browser.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+REM Check if Firefox is installed as an x86 or x64 application.
+IF EXIST "C:\Program Files\Mozilla Firefox\uninstall\helper.exe" (GOTO x64)
+IF EXIST "C:\Program Files (x86)\Mozilla Firefox\uninstall\helper.exe" (GOTO x86)
+GOTO end
+
+:x64
+"C:\Program Files\Mozilla Firefox\uninstall\helper.exe" /SetAsDefaultAppGlobal
+GOTO end
+
+:x86
+"C:\Program Files (x86)\Mozilla Firefox\uninstall\helper.exe" /SetAsDefaultAppGlobal
+GOTO end
+
+:end
+EXIT
